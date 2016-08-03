ae7528aa5d0ad24999ea550134dc114a6aa245f5
Dielentheis <elizabeth.dielentheis@gmail.com>
2016-08-03 10:51:04 -0400

    added socket.io

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var swig = require('swig');
 var routes = require('./routes/');
+var socketio = require('socket.io');
+// ...
+var server = app.listen(3000);
+var io = socketio.listen(server);
 
 swig.setDefaults({ cache: false });
 
@@ -9,13 +13,5 @@ app.set('views', './views'); // point res.render to the proper directory
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', swig.renderFile); // when giving html files to res.render, tell it to use swig
 
-
-//app.use('/', function(req, res, next) {
-//	res.render('index', locals);
-
-//});
-
-app.use('/', routes);
-
-app.listen(3000);
+app.use('/', routes(io));
 
--- routes/index.js
+++ routes/index.js
@@ -1,50 +1,50 @@
-var express = require('express');
-var router = express.Router();
-var bodyParser = require('body-parser');
-// could use one line instead: var router = require('express').Router();
-var tweetBank = require('../tweetBank');
+module.exports = function(io) {
+	var express = require('express');
+	var router = express.Router();
+	var bodyParser = require('body-parser');
+	// could use one line instead: var router = require('express').Router();
+	var tweetBank = require('../tweetBank');
 
-router.use(bodyParser.urlencoded({extended: true}));
+	router.use(bodyParser.urlencoded({extended: true}));
 
-router.get('/', function (req, res) {
-  var tweets = tweetBank.list();
-  var showForm = true;
-  res.render( 'index', { tweets: tweets, showForm: true } );
-});
+	router.get('/', function (req, res) {
+	  var tweets = tweetBank.list();
+	  var showForm = true;
+	  res.render( 'index', { tweets: tweets, showForm: true } );
+	});
 
-router.get('/users/:name', function (req, res) {
-  var name = req.params.name;
-  var tweets = tweetBank.find( {name: name} );
-  if (tweets.length == 0) {
-  	tweets = [{name: 'Error', content: 'No tweets by specified user.'}];
-  }
-  console.log(name);
-  res.render( 'index', { tweets: tweets, showForm: true, name: name } );
-});
+	router.get('/users/:name', function (req, res) {
+	  var name = req.params.name;
+	  var tweets = tweetBank.find( {name: name} );
+	  if (tweets.length == 0) {
+	  	tweets = [{name: 'Error', content: 'No tweets by specified user.'}];
+	  }
+	  res.render( 'index', { tweets: tweets, showForm: true, name: name } );
+	});
 
-router.get('/tweets/:id', function (req, res) {
-  var id = req.params.id;
-  var tweets = tweetBank.find( function(element) {
-  	return element.id == id;
-  } );
-  console.log(tweets);
-  if (tweets.length == 0) {
-  	tweets = [{name: 'Error', content: 'No tweets with specified ID.'}];
-  }
-  res.render( 'index', { tweets: tweets } );
-});
+	router.get('/tweets/:id', function (req, res) {
+	  var id = req.params.id;
+	  var tweets = tweetBank.find( function(element) {
+	  	return element.id == id;
+	  } );
+	  if (tweets.length == 0) {
+	  	tweets = [{name: 'Error', content: 'No tweets with specified ID.'}];
+	  }
+	  res.render( 'index', { tweets: tweets } );
+	});
 
-router.post('/tweets', function(req, res) {
-	console.log(req.body);
-  var name = req.body.name;
-  var text = req.body.text;
-  tweetBank.add(name, text);
-  res.redirect('/');
-});
 
-router.use(express.static('public'));
-router.use(express.static('routes'));
-router.use(express.static('views'));
-router.use(express.static('node_modules'));
+	router.use(express.static('public'));
+	router.use(express.static('routes'));
+	router.use(express.static('views'));
+	router.use(express.static('node_modules'));
 
-module.exports = router;
+	router.post('/tweets', function(req, res) {
+	  var name = req.body.name;
+	  var text = req.body.text;
+	  tweetBank.add(name, text);
+	  io.sockets.emit('newTweet', { name: 'Steven U.', content: 'Pearl has a pointy nose' });
+	  res.redirect('/');
+	});
+	return router;
+};
